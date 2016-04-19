c3ae3a8df9764086298da211126d3921a0bd0d37
Matthew Farrell <farrellm@alum.mit.edu>
2016-04-18 21:52:36 -0400

    add ArrowPlus and ArrowChoice instances

--- src/Data/Conduit/Arrow/Internal.hs
+++ src/Data/Conduit/Arrow/Internal.hs
@@ -10,16 +10,6 @@ import qualified Data.Conduit.Combinators as CC
 import           Data.Either
 
 newtype ConduitArrow m i o = ConduitArrow {getConduit :: Conduit i m o}
-splitC :: Monad m => Conduit (Maybe (Either a b)) m [Either a b]
-splitC = s []
-  where s ls = do
-          ml <- await
-          case ml of
-            Nothing -> return ()
-            Just Nothing -> do
-              yield $ reverse ls
-              s []
-            Just (Just l) -> s $ l:ls
 
 instance (Monad m) => Category (ConduitArrow m) where
   id = ConduitArrow $ awaitForever yield
@@ -32,10 +22,33 @@ instance Monad m => Arrow (ConduitArrow m) where
     getZipConduit (ZipConduit (CC.map fst =$= c =$= CC.map (Just . Left)) <*
                    ZipConduit (CC.map snd =$= CC.map (Just . Right)) <*
                    ZipConduit (CC.map $ const Nothing)) =$=
-    splitC =$=
+    split [] =$=
     CC.map partitionEithers =$=
     CC.concatMap (uncurry zip)
+    where split ls = do
+            ml <- await
+            case ml of
+              Nothing       -> return ()
+              Just Nothing  -> yield (reverse ls) >> split []
+              Just (Just l) -> split $ l:ls
 
   -- second (ConduitArrow c) = ConduitArrow $
   -- (&&&) (ConduitArrow c1) (ConduitArrow c2) = ConduitArrow $
   -- (***) (ConduitArrow c1) (ConduitArrow c2) = ConduitArrow $
+
+
+instance Monad m => ArrowZero (ConduitArrow m) where
+  zeroArrow = ConduitArrow $ pure ()
+
+instance Monad m => ArrowPlus (ConduitArrow m) where
+  (<+>) (ConduitArrow a) (ConduitArrow b) = ConduitArrow $
+    getZipConduit (ZipConduit a <* ZipConduit b)
+
+
+instance Monad m => ArrowChoice (ConduitArrow m) where
+  left (ConduitArrow a) = ConduitArrow $
+    getZipConduit
+    ( ZipConduit (CC.filter isLeft =$= CC.map getLeft =$= a =$= CC.map Left) <*
+      ZipConduit (CC.filter isRight =$= CC.map (Right .getRight)))
+    where getLeft (Left v) = v
+          getRight (Right v) = v
